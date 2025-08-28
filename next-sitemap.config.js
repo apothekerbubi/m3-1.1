b957c8ee6e4281aec42ce8759d04d6a39b3d3047
apothekerbubi <151027878+apothekerbubi@users.noreply.github.com>
2025-08-28 17:30:20 +0200

    link

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,9 @@
-/** @type {import('next-sitemap').IConfig} */
+// next-sitemap.config.js
 module.exports = {
-  siteUrl: 'https://exasim.de',  // ✅ Deine Domain hier eintragen
+  siteUrl: 'https://www.exasim.de',
   generateRobotsTxt: true,
+  exclude: ['*'], // ← verhindert, dass automatisch Seiten eingesammelt werden
+  additionalPaths: async (config) => [
+    await config.transform(config, '/'), // ← nur Startseite eintragen
+  ],
 }
