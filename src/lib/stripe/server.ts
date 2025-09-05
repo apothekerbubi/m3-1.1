2958102e8d4c6342e39a81e049a419db2d45789e
apothekerbubi <151027878+apothekerbubi@users.noreply.github.com>
2025-09-05 21:36:02 +0200

    Update server.ts

--- src/lib/stripe/server.ts
+++ src/lib/stripe/server.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 
 // Node.js-Stripe-Instanz für Server-Routen
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2025-08-27.basil",
 });
 
 export default stripe;
