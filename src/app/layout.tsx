23cccaa851fe07908096fdc97388cf5f7af9d617
apothekerbubi <151027878+apothekerbubi@users.noreply.github.com>
2025-09-24 11:21:21 +0200

    google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,14 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="de" className={jakarta.variable}>
       <head>
+         {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5GB3NKJV');`}
+        </Script>
         <Script
            type="text/javascript"
           src="https://cdn.consentmanager.net/delivery/autoblocking/82049fe772b7e.js"
@@ -36,6 +44,15 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         />
       </head>
       <body className="bg-[var(--bg)] text-[var(--fg)]">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5GB3NKJV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <LayoutVars>
            
           <AnalyticsConsent />
